69012388f197f83bce42ef8743a0caa6fb99cb6b
Ashymad <czilukim@o2.pl>
2018-11-28 14:04:05 +0100

    WIP: cleanup+plots

--- code/zad.py
+++ code/zad.py
@@ -1,55 +1,65 @@
 import numpy as np
+from numpy import exp
 import sympy as sp
-import pymc3 as pm
 from sympy.abc import i,x
-from matplotlib import pyplot as ppl
-import theano.tensor as tt
+import pymc3 as pm
 import seaborn as sns
+import matplotlib as mpl
+mpl.use("pgf")
+import matplotlib.pyplot as ppl
+import platform, locale
 
-K = lambda x: sp.exp(-(x**2)/2)/sp.sqrt(2*sp.pi)
+if platform.system() == 'Windows':
+        locale.setlocale(locale.LC_NUMERIC, 'Polish')
+else:
+        locale.setlocale(locale.LC_NUMERIC, ('pl_PL', 'UTF-8'))
+
+r29 = np.loadtxt("rok2009.txt")
+exec(open("prior2008LDWN.py").read())
+exec(open("prior2008LN.py").read())
 
 n = 10
 
-r29 = np.loadtxt("rok2009.txt")
-
 Ldwnn = np.random.choice(r29[:,0],n)
-Ln = np.random.choice(r29[:,1],n)
-
-h = 1.06*np.std(Ldwnn)*n**(-1/5)
+Lnn = np.random.choice(r29[:,1],n)
 
 Ldwn = sp.Array(Ldwnn)
-
-h = sp.symbols('h')
-
-f = lambda x, h: 1/(n*h)*sp.Sum(K((x - Ldwn[i-1])/h), (i, 1, n)).doit()
-
-#ran = (x, np.min(Ldwn)-1/h, np.max(Ldwn)+1/h)
-#
-#p = sp.plot(f(x), ran, show=False)
-#
-#for xi in range(n):
-#    p.extend(sp.plot(K((x-Ldwn[xi])/h)/(n*h), ran, line_color='r', show=False))
-#
-#ppl.show(block=False)
+Ln = sp.Array(Lnn)
 
 
-from numpy import exp
-exec(open("prior2008LDWN.m").read())
+def K(x):
+    return sp.exp(-(x**2)/2)/sp.sqrt(2*sp.pi)
 
-f_lbd = sp.lambdify((x, h), f(x, h))
 
+def f(x, xo, K, h=None):
+    n = len(xo)
+    if h is None:
+        h = 1.06*xo.std()*n**(-1/5)
+    return 1/(n*h)*sp.Sum(K((x - xo[i-1])/h), (i, 1, n)).doit()
+
+ran = (x, np.min(Ldwn)-1/h, np.max(Ldwn)+1/h)
+
+p = sp.plot(f(x), ran, show=False)
+
+for xi in range(n):
+    p.extend(sp.plot(K((x-Ldwn[xi])/h), ran, line_color='r', show=False))
+
+ppl.savefig("../report/plots/Kernel_Ldwn.pgf")
+
+f_lbd = sp.lambdify(x, f(x))
 
 def logp_theta(value):
     return np.log(prior2008LDWN(value)).sum()
 
-def logp_est(value, h):
+def logp_est(value):
     return np.log(f_lbd(value, h)).sum()
+
+
 
 with pm.Model() as model:
     theta = pm.DensityDist('theta', logp_theta, testval=np.mean(Ldwnn))
-    h = 1.06*theta.std()*n**(-1/5) + 1e-10
-    like = pm.DensityDist('y_est', logp_est, observed={'value': Ldwnn, 'h': h})
-    trace = pm.sample(100000, nuts_kwargs={'target_accept': 0.9})
+    like = pm.DensityDist('y_est', logp_est, observed={'value': Ldwnn})
+    trace = pm.sample(100000, nuts_kwargs={'target_accept': 0.9}, tune=1000)
 
 pl = sns.distplot(trace.get_values('theta'))
 xx = pl.lines[0].get_xdata()
